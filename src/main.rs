0cec08198ade73bb49cbbd00355932ba925e16bf
Murilo <127051508+murilo587@users.noreply.github.com>
2025-02-14 13:12:00 -0300

    feat: options menu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut choice = String::new();
+        println!("MENU");
+        println!("1. List all files of the current folder");
+        println!("2. Exit");
+
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to read input");
+
+        let choice: u32 = match choice.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input, please enter a number");
+                continue;
+            }
+        };
+
+        match choice {
+            1 => {
+                //function to list all files
+            }
+            2 => {
+                println!("Exiting...");
+                break;
+            }
+            _ => println!("Invalid choice, try again!"),
+        }
+    }
 }
