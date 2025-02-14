6d419f1524ed779224628e010758467f26b8dfd9
Murilo <127051508+murilo587@users.noreply.github.com>
2025-02-14 15:51:53 -0300

    feat: edit_file function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::{self, File};
-use std::io::{self, Write};
+use std::io::{self, Write, Read};
 use std::path::Path;
 
 fn main() {
@@ -8,6 +8,7 @@ fn main() {
         println!("MENU");
         println!("1. List all files of the current folder");
         println!("2. Create new file");
+        println!("3. Edit an existing file");
 
         io::stdin()
             .read_line(&mut choice)
@@ -40,6 +41,18 @@ fn main() {
                 let content = content.trim();
 
                 create_file("src/files/.", file_name, content);
+            }
+            3 => {
+                let mut file_name = String::new();
+
+                println!("Enter file name (with extension):");
+                io::stdin()
+                    .read_line(&mut file_name)
+                    .expect("Invalid input");
+
+                let file_name = file_name.trim();
+
+                edit_file("src/files/.", file_name);
             }
             _ => break,
         }
@@ -74,5 +87,55 @@ fn main() {
         }
 
         println!("\x1b[32mDone!\x1b[0m");
+    }
+
+    fn edit_file(directory: &str, file_name: &str) {
+        let path = Path::new(directory).join(file_name);
+        
+        if !path.exists() {
+            println!("\x1b[31mFile does not exist\x1b[0m");
+            return;
+        }
+    
+        let mut current_content = String::new();
+        let mut file = match File::open(&path) {
+            Ok(file) => file,
+            Err(_) => {
+                println!("\x1b[31mFailed to open file for editing\x1b[0m");
+                return;
+            }
+        };
+    
+        if let Err(_) = file.read_to_string(&mut current_content) {
+            println!("\x1b[31mFailed to read content from file\x1b[0m");
+            return;
+        }
+
+        println!("Current content of '{}':", file_name);
+        println!("{}", current_content);
+    
+        let mut new_content = String::new();
+        println!("Enter new content (or press 'Enter' to keep the current content):");
+        io::stdin().read_line(&mut new_content).expect("Invalid input");
+
+        if new_content.trim().is_empty() {
+            println!("\x1b[32mFile remains unchanged!\x1b[0m");
+            return;
+        }
+
+        let mut file = match File::create(&path) {
+            Ok(file) => file,
+            Err(_) => {
+                println!("\x1b[31mFailed to open file for editing\x1b[0m");
+                return;
+            }
+        };
+    
+        if let Err(_) = file.write_all(new_content.as_bytes()) {
+            println!("\x1b[31mFailed to write new content to file\x1b[0m");
+            return;
+        }
+    
+        println!("\x1b[32mFile edited successfully!\x1b[0m");
     }
 }
